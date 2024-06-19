6671f336ac6aa63c1444e8c04fe406a0a51d45b4
rohitt99 <164199173+rohitt99@users.noreply.github.com>
2024-06-19 22:34:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6824943579:AAF5GUHPUA5nDZuspI1c24HZr3ywXOMPKI0")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", 25830228))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "a23a5133bddbdab87df3df06ccf63a89")
 
-    AUDIO_THUMBNAIL = os.environ.get("AUDIO_THUMBNAIL", "")
+    AUDIO_THUMBNAIL = os.environ.get("AUDIO_THUMBNAIL", "https://graph.org/file/350c9c541ecf4f2c67070.jpg")
 
-    VIDEO_THUMBNAIL = os.environ.get("VIDEO_THUMBNAIL", "")
+    VIDEO_THUMBNAIL = os.environ.get("VIDEO_THUMBNAIL", "https://graph.org/file/fc3fff668765511b35f5b.jpg")
